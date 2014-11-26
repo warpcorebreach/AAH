7b8a7bf77967dee735a6b2d55f894bbe80c7e3bf
jtrimm3 <jtrimm3@gatech.edu>
2014-11-25 23:09:12 -0500

    Updated payInfo

--- AAH/src/aah/PayInfoController.java
+++ AAH/src/aah/PayInfoController.java
@@ -6,8 +6,16 @@
 
 package aah;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -21,27 +29,19 @@ import javafx.scene.control.TextField;
  * @author Julianna
  */
 public class PayInfoController implements Initializable {
-
-    @FXML
-    private Label cardName = new Label();
+    
+    private Connection conn;
+    private String curUser;
+    private List<Integer> cards;
     
     @FXML
     private TextField cardNameText = new TextField();
     
     @FXML
-    private Label cardNumber = new Label();
-    
-    @FXML
     private TextField cardNumberText = new TextField();
-    
-    @FXML
-    private Label expLabel = new Label();
 
     @FXML
     private ChoiceBox expDate = new ChoiceBox();
-
-    @FXML
-    private Label cvv = new Label();
     
     @FXML
     private TextField cvvText = new TextField();
@@ -54,10 +54,49 @@ public class PayInfoController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        cardName.setText("Name on the card: ");
-        cardNumber.setText("Card Number: ");
-        expLabel.setText("Expiration Date: ");
-        cvv.setText("CVV: ");
+        conn = Tables.getConnection();
+        curUser = Tables.getCurrentUser();
+        cards = new ArrayList<>();
+        try {
+            String getQ = "SELECT Card_No" +
+                            "FROM Payment_Info" +
+                            "WHERE Username = '" + curUser + "'";
+            Statement getCard = conn.createStatement();
+            ResultSet get = getCard.executeQuery(getQ);
+            while (get.next()) {
+                cards.add(get.getInt("Card_No"));
+            }
+        } catch(SQLException ex) {
+            System.out.println("SQL Error: " + ex.getMessage());
+        }
     }
     
+    @FXML
+    private void saveCard(ActionEvent event)throws IOException, SQLException {
+        String cardNo = cardNumberText.getText();
+        int cardNoInt = Integer.parseInt(cardNo);
+        String cvv = cvvText.getText();
+        int cvvInt = Integer.parseInt(cvv);
+        String cardName = cardNameText.getText();
+        String addQ = "INSERT INTO Payment_Info VALUES"
+                        +"('" + cardNoInt + "', '" + cvvInt + "', '" + cardName + ")";
+                        //+ "', '" + Exp_Date + "', '" + curUser + ")";
+
+        Statement newCard = conn.createStatement();
+        newCard.executeUpdate(addQ);
+        newCard.close();
+
+        System.out.println("Card added.");
+    }
+    
+    @FXML
+    private void deleteCard(ActionEvent event)throws IOException, SQLException {
+        String delQ = "DELETE FROM Payment_Info";// +
+                        //+ "WHERE Card_No = '" + $card_no + "'";
+        Statement delCard = conn.createStatement();
+        delCard.executeUpdate(delQ);
+        delCard.close();
+
+        System.out.println("Card deleted.");
+    }
 }
