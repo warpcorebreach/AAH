688215467fad220820a28b5dceca80b2cc789bbc
jtrimm3 <jtrimm3@gatech.edu>
2014-11-28 01:41:06 -0500

    Update the rest of  the table views for the most part

--- AAH/src/aah/AppReviewController.java
+++ AAH/src/aah/AppReviewController.java
@@ -5,9 +5,28 @@
  */
 package aah;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,13 +34,103 @@ import javafx.fxml.Initializable;
  * @author Justin
  */
 public class AppReviewController implements Initializable {
+    
+    @FXML
+    private Button nextButton = new Button();
+    
+    @FXML
+    private TableView table = new TableView(); 
+    
+    private ObservableList<ObservableList> data;  
 
     /**
      * Initializes the controller class.
      */
+    @FXML
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        data = FXCollections.observableArrayList();
+        try {   //we should fix this query AND WHILE LOOP
+            String revQ = "SELECT Name, DOB, Gender, Income, Category, Min_Rent, " +
+                            "Max_Rent, Move_Date, Lease_Term, Username " +
+                            "FROM Prospective_Resident " +
+                            "WHERE Prospective_Resident.Username NOT IN (SELECT Username FROM " +
+                            "	Resident) " +
+                            "AND ((Income/3) >= SELECT MAX(Rent) FROM Apartment " +
+                            "WHERE Apartment.Category = Prospective_Resident.Category) " +
+                            "AND (Move_Date >= SELECT MIN(Available_On) FROM Apartment WHERE " +
+                            "Apartment.Category = Prospective_Resident.Category);";
+            Connection conn = Tables.getConnection();
+            Statement getRev = conn.createStatement();
+            ResultSet rev = getRev.executeQuery(revQ);
+            while (rev.next()) {
+                String name = rev.getString("Name");
+                Date dob = rev.getDate("DOB");
+                String Gen = rev.getString("Gender");
+                int income = rev.getInt("Income");
+                String cat = rev.getString("Category");
+                int min = rev.getInt("Min_Rent");
+                int max = rev.getInt("Max_Rent");
+                Date move = rev.getDate("Move_Date");
+                String term = rev.getString("Lease_Term");
+                String user = rev.getString("Username");
+                CheckBox check = new CheckBox();
+                
+                ObservableList<String> row = FXCollections.observableArrayList();  
+                for(int i=1 ; i<=rev.getMetaData().getColumnCount(); i++){                      
+                    row.add(rev.getString(i));  
+                }
+                data.add(row); 
+            }
+            table.setItems(data); 
+            TableColumn nameCol = new TableColumn("Name");  
+            nameCol.setMinWidth(100);  
+
+            TableColumn dobCol = new TableColumn("Date of Birth");  
+            dobCol.setMinWidth(100);          
+
+            TableColumn genCol = new TableColumn("Gender");  
+            genCol.setMinWidth(100);  
+            
+            TableColumn inCol = new TableColumn("Monthly Income($)");  
+            inCol.setMinWidth(100);  
+            
+            TableColumn typeCol = new TableColumn("Type of Apartment Requested");  
+            typeCol.setMinWidth(100);  
+
+            TableColumn moveCol = new TableColumn("Preferred Move-in Date");  
+            moveCol.setMinWidth(100);          
+
+            TableColumn leCol = new TableColumn("Lease Term");  
+            leCol.setMinWidth(100);  
+            
+            TableColumn rejCol = new TableColumn("Reject/Accept");  
+            rejCol.setMinWidth(100); 
+            
+            TableColumn checkCol = new TableColumn("");  
+            checkCol.setMinWidth(100); 
+
+            table.getColumns().addAll(nameCol, dobCol, genCol, inCol, typeCol, 
+                    moveCol, leCol, rejCol, checkCol);
+         
+            System.out.println("Table Value::" + table); 
+            
+        } catch (SQLException ex) {
+            System.out.println("SQL Error: " + ex.getMessage());
+        }
+    }
+    
+    @FXML
+    private void next(ActionEvent event)throws IOException, SQLException {
+        
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root;
+        root = FXMLLoader.load(
+                getClass().getResource("AptAllotment.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
--- AAH/src/aah/AptAllotmentController.java
+++ AAH/src/aah/AptAllotmentController.java
@@ -6,9 +6,29 @@
 
 package aah;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,13 +36,114 @@ import javafx.fxml.Initializable;
  * @author Julianna
  */
 public class AptAllotmentController implements Initializable {
+    
+    @FXML
+    private Button allotButton = new Button();
+    
+    @FXML
+    private Label nameLabel = new Label();
+    
+    @FXML
+    private TableView table = new TableView(); 
+    
+    private ObservableList<ObservableList> data;   
 
     /**
      * Initializes the controller class.
      */
+    @FXML
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        data = FXCollections.observableArrayList();
+        try {
+            String allQ = "SELECT (Apt_No, APARTMENT.Category, Rent, Sq_Ft, Available_On, Name) " +
+                            "FROM APARTMENT JOIN PROSPECTIVE_RESIDENT " +
+                            "	ON APARTMENT.Category = PROSPECTIVE_RESIDENT.Category " +
+                            "WHERE (Rent >= Min_Rent AND Rent <= Max_Rent AND Move_Date >= Available_On " +
+                            "	AND Pref_Lease = Lease_Term AND Username = $username) " +
+                            "ORDER BY Rent DESC;";
+            Connection conn = Tables.getConnection();
+            Statement getAll = conn.createStatement();
+            ResultSet allot = getAll.executeQuery(allQ);
+            while (allot.next()) {
+                int apt = allot.getInt("Apt_No");
+                String cat = allot.getString("Category");
+                int rent = allot.getInt("Rent");
+                int feet = allot.getInt("Sq_Ft");
+                Date avail = allot.getDate("Available_On");
+                String name = allot.getString("Name");
+                CheckBox check = new CheckBox();
+                
+                ObservableList<String> row = FXCollections.observableArrayList();  
+                for(int i=1 ; i<=allot.getMetaData().getColumnCount(); i++){                      
+                    row.add(allot.getString(i));  
+                }
+                data.add(row); 
+            }
+            table.setItems(data); 
+            TableColumn aptCol = new TableColumn("Apartment No");  
+            aptCol.setMinWidth(100);  
+
+            TableColumn catCol = new TableColumn("Category");  
+            catCol.setMinWidth(200);          
+
+            TableColumn rentCol = new TableColumn("Monthly Rent($)");  
+            rentCol.setMinWidth(100);  
+            
+            TableColumn feetCol = new TableColumn("Sq Ft.");  
+            feetCol.setMinWidth(100); 
+            
+            TableColumn dateCol = new TableColumn("Available From");  
+            dateCol.setMinWidth(200); 
+            
+            TableColumn checkCol = new TableColumn("");  
+            checkCol.setMinWidth(100); 
+
+            table.getColumns().addAll(aptCol, catCol, rentCol, feetCol, dateCol, checkCol);  
+            
+            try {
+                ActionEvent event = null;
+                Node node = (Node) event.getSource();
+                Stage stage = (Stage) node.getScene().getWindow();
+                Parent root;
+                root = FXMLLoader.load(
+                        getClass().getResource("AptAllotment.fxml"));
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException ex) {
+                System.out.println("IO Error: " + ex.getMessage());
+            }
+            System.out.println("Table Value::" + table); 
+            
+        } catch (SQLException ex) {
+            System.out.println("SQL Error: " + ex.getMessage());
+        }
+    }  
+    
+    @FXML
+    private void assign(ActionEvent event)throws IOException, SQLException {
+        String assQ = "INSERT INTO Resident VALUES ($username, $apt_no);";
+        Connection conn = Tables.getConnection();
+        Statement assRes = conn.createStatement();
+        assRes.executeUpdate(assQ);
+        assRes.close(); 
+        
+        String upQ = "UPDATE APARTMENT " +
+                        "SET Available_On = " +
+                        "DATE_ADD((SELECT Available_On " +
+                        " FROM APARTMENT A JOIN RESIDENT R " +
+                        " ON A.Apt_No = R.Apt_No " +
+                        " WHERE Username = $username), " +
+                        "INTERVAL (SELECT Pref_Lease " +
+                        " FROM APARTMENT A JOIN RESIDENT R " +
+                        " ON A.Apt_No = R.Apt_No\n" +
+                        " WHERE Username = $username) MONTH) " +
+                        "WHERE Apt_No = $apt_no;";
+        Statement upRes = conn.createStatement();
+        upRes.executeUpdate(upQ);
+        upRes.close();
+        conn.close();
+    }
     
 }
